8d33e5d29f7311f659d5de9369646b2f9582a438
carternadain <cnadain@gmail.com>
2022-05-05 10:54:49 -0700

    comment model

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        post_id: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+
+    {
+        sequelize,
+        // timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
